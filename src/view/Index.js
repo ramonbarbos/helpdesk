b47a61c1c6546f5a23ca3db8b2eb02223e5ccaf3
Ramon Barbosa <ramonlegendario21@gmail.com>
2023-07-07 10:16:02 -0300

    modal chamado

--- src/view/Index.js
+++ src/view/Index.js
@@ -1,31 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
 import { FontAwesome, Entypo, MaterialIcons } from '@expo/vector-icons';
-import { Box, Text, Center, Input, Heading, FormControl, VStack, Icon, Button, Checkbox, HStack, Image, Pressable, Stack, FlatList } from "native-base";
-
+import React, { useState, useEffect } from 'react';
 import { useNavigation } from '@react-navigation/native';
-import { useEffect, useState } from 'react';
-
+import { Box, Text,  Heading, VStack, Icon, Button,  HStack,Pressable, Stack, FlatList, Modal } from 'native-base';
 
 export default function Index() {
   const navigation = useNavigation();
   const [aberto, setAberto] = useState([]);
   const [fechado, setFechado] = useState([]);
-  const [chamadosAbertos, setChamadosAbertos] = useState(true); // Estado para controlar a exibição de chamados abertos ou fechados
-  const [quantidadeChamados, setQuantidadeChamados] = useState(0); // Estado para controlar a quantidade de chamados
+  const [chamadosAbertos, setChamadosAbertos] = useState(true);
+  const [quantidadeChamados, setQuantidadeChamados] = useState(0);
+  const [modalVisible, setModalVisible] = useState(false);
+  const [chamadoSelecionado, setChamadoSelecionado] = useState(null);
+  const initialRef = React.useRef(null);
+  const finalRef = React.useRef(null);
 
   const fetchAberto = async () => {
-    const url = `http://10.0.0.120/apiHelpdesk/chamado/aberto`;
+    const url = 'http://10.0.0.120/apiHelpdesk/chamado/aberto';
     try {
       const response = await fetch(url);
       const responseData = await response.json();
 
       if (responseData && responseData.resposta) {
-    
-       
-        if (responseData.tipo === "sucesso" ) {
+        if (responseData.tipo === 'sucesso') {
           setAberto(responseData.resposta);
-        }else if(responseData.tipo === "erro"){
-          console.log('sem registro')
+        } else if (responseData.tipo === 'erro') {
+          console.log('sem registro');
         }
       }
     } catch (error) {
@@ -34,28 +34,30 @@ export default function Index() {
   };
 
   const fetchFechado = async () => {
-    const url = `http://10.0.0.120/apiHelpdesk/chamado/fechado`;
+    const url = 'http://10.0.0.120/apiHelpdesk/chamado/fechado';
     try {
       const response = await fetch(url);
       const responseData = await response.json();
 
-      if (responseData.tipo === "sucesso" ) {
+      if (responseData.tipo === 'sucesso') {
         setFechado(responseData.resposta);
-      }else if(responseData.tipo === "erro"){
-        console.log('sem registro')
+      } else if (responseData.tipo === 'erro') {
+        console.log('sem registro');
       }
     } catch (error) {
       console.error(error);
     }
   };
 
-  const handleChamadosPress = () => {
+  const handleChamadosPress = (chamado) => {
+    setChamadoSelecionado(chamado);
+    setModalVisible(true);
     console.log('Visualizar Chamados');
   };
 
   const handleFloatingButtonPress = () => {
     console.log('Criar Chamado');
-    navigation.navigate("Novo Chamado");
+    navigation.navigate('Novo Chamado');
   };
 
   const handleOpenPress = () => {
@@ -81,9 +83,7 @@ export default function Index() {
       }
     }, 5000);
 
-
     return () => clearInterval(intervalId);
-    
   }, [chamadosAbertos]);
 
   useEffect(() => {
@@ -92,25 +92,32 @@ export default function Index() {
     setQuantidadeChamados(quantidade);
   }, [aberto, fechado, chamadosAbertos]);
 
-
   const renderUserItem = ({ item }) => (
     <Box>
-      <Pressable mt={2} onPress={() => handleChamadosPress()}>
+      <Pressable mt={2} onPress={() => handleChamadosPress(item)}>
         {({ isHovered, isFocused, isPressed }) => (
-          <Box w={'full'} h={20} borderColor="black" borderRadius={15} bg={isPressed ? '#fff' : isHovered ? '#fff' : '#fff'} style={{ transform: [{ scale: isPressed ? 0.96 : 1 }] }}>
-           
-           
-           
-           <HStack h={'full'} borderTopLeftRadius={15} borderBottomLeftRadius={15}>
-            <Box h={'full'} w={2} bg={chamadosAbertos ? '#04B2D9' : '#CF0620'} borderTopLeftRadius={15} borderBottomLeftRadius={15}></Box>
+          <Box
+            w="full"
+            h={20}
+            borderColor="black"
+            borderRadius={15}
+            bg={isPressed || isHovered ? '#fff' : '#fff'}
+            style={{ transform: [{ scale: isPressed ? 0.96 : 1 }] }}
+          >
+            <HStack h="full" borderTopLeftRadius={15} borderBottomLeftRadius={15}>
+              <Box
+                h="full"
+                w={2}
+                bg={chamadosAbertos ? '#04B2D9' : '#CF0620'}
+                borderTopLeftRadius={15}
+                borderBottomLeftRadius={15}
+              ></Box>
               <VStack p={2}>
-                <Heading size={'sm'}>{item.id}</Heading>
-                <Heading size={'sm'}>{item.titulo}</Heading>
-                <Text size={'sm'}>{item.descricao.substring(0, 42)}...</Text>
+                <Heading size="sm">#{item.id}</Heading>
+                <Heading size="sm">{item.titulo}</Heading>
+                <Text size="sm">{item.descricao.substring(0, 42)}...</Text>
               </VStack>
             </HStack>
-          
-           
           </Box>
         )}
       </Pressable>
@@ -118,8 +125,8 @@ export default function Index() {
   );
 
   return (
-    <Box w={'full'} p={6}>
-      <HStack h={'20%'} mt={4} alignItems={'center'} justifyContent={'space-between'}>
+    <Box w="full" p={6}>
+      <HStack h="20%" mt={4} alignItems="center" justifyContent="space-between">
         <VStack>
           <Heading>Helpdesk</Heading>
           <Text>Registre o chamado!</Text>
@@ -127,49 +134,63 @@ export default function Index() {
         <FontAwesome name="user-circle" size={35} color="black" />
       </HStack>
 
-      <Stack h={'20%'} justifyContent={'center'} alignItems={'center'}>
+      <Stack h="20%" justifyContent="center" alignItems="center">
         <HStack>
-          <Pressable onPress={() => handleOpenPress()}>
+          <Pressable onPress={handleOpenPress}>
             {({ isHovered, isFocused, isPressed }) => (
-              <Box bg={'#04B2D9'} w={140} h={9} style={{ transform: [{ scale: isPressed ? 0.96 : 1 }] }} borderTopLeftRadius={10} borderBottomLeftRadius={10} justifyContent={'center'} alignItems={'center'}>
-                <Heading size={'sm'} color={'white'}>Abertos</Heading>
+              <Box
+                bg="#04B2D9"
+                w={140}
+                h={9}
+                style={{ transform: [{ scale: isPressed ? 0.96 : 1 }] }}
+                borderTopLeftRadius={10}
+                borderBottomLeftRadius={10}
+                justifyContent="center"
+                alignItems="center"
+              >
+                <Heading size="sm" color="white">
+                  Abertos
+                </Heading>
               </Box>
             )}
           </Pressable>
-          <Pressable onPress={() => handleClosePress()}>
+          <Pressable onPress={handleClosePress}>
             {({ isHovered, isFocused, isPressed }) => (
-              <Box bg={'#CF0620'} style={{ transform: [{ scale: isPressed ? 0.96 : 1 }] }} w={140} h={9} borderTopRightRadius={10} borderBottomRightRadius={10} justifyContent={'center'} alignItems={'center'}>
-                <Heading size={'sm'} color={'white'}>Fechados</Heading>
+              <Box
+                bg="#CF0620"
+                style={{ transform: [{ scale: isPressed ? 0.96 : 1 }] }}
+                w={140}
+                h={9}
+                borderTopRightRadius={10}
+                borderBottomRightRadius={10}
+                justifyContent="center"
+                alignItems="center"
+              >
+                <Heading size="sm" color="white">
+                  Fechados
+                </Heading>
               </Box>
             )}
           </Pressable>
         </HStack>
       </Stack>
 
-      <Stack h={'60%'} >
-        <HStack justifyContent={'space-between'} w={'full'}>
-          <Heading size={'sm'}>Chamados</Heading>
-          <Heading size={'sm'}>{quantidadeChamados}</Heading>
+      <Stack h="60%">
+        <HStack justifyContent="space-between" w="full">
+          <Heading size="sm">Chamados</Heading>
+          <Heading size="sm">N°{quantidadeChamados}</Heading>
         </HStack>
 
-          <Box mt={4} mb={9}>
+        <Box mt={4} mb={9}>
           <FlatList
-                    data={chamadosAbertos ? aberto : fechado} // Exibe a lista de chamados abertos ou fechados de acordo com o estado
-                    renderItem={renderUserItem}
-                    keyExtractor={(item) => item.id.toString()}
-                  />
-          </Box>
-      
-
+            data={chamadosAbertos ? aberto : fechado} // Exibe a lista de chamados abertos ou fechados de acordo com o estado
+            renderItem={renderUserItem}
+            keyExtractor={(item) => item.id.toString()}
+          />
+        </Box>
       </Stack>
 
-      <Pressable
-        position="absolute"
-        bottom={6}
-        right={6}
-        zIndex={10}
-        onPress={handleFloatingButtonPress}
-      >
+      <Pressable position="absolute" bottom={6} right={6} zIndex={10} onPress={handleFloatingButtonPress}>
         {({ isPressed, isHovered }) => (
           <Box
             bg={isPressed || isHovered ? '#04B2F8' : '#04B2D9'}
@@ -181,6 +202,27 @@ export default function Index() {
           </Box>
         )}
       </Pressable>
+
+      <Modal isOpen={modalVisible} onClose={() => setModalVisible(false)} initialFocusRef={initialRef} finalFocusRef={finalRef}>
+        <Modal.Content>
+          <Modal.CloseButton />
+          <Modal.Header>Opções</Modal.Header>
+          <Modal.Body>
+            {chamadoSelecionado && (
+              <>
+                <Text>{chamadoSelecionado.id}</Text>
+                <Text>{chamadoSelecionado.titulo}</Text>
+                <Text>{chamadoSelecionado.descricao}</Text>
+              </>
+            )}
+          </Modal.Body>
+          <Modal.Footer>
+            <Button.Group space={2}>
+              <Button onPress={() => setModalVisible(false)}>Sair</Button>
+            </Button.Group>
+          </Modal.Footer>
+        </Modal.Content>
+      </Modal>
     </Box>
   );
 }
